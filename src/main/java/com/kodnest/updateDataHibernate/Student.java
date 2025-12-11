db4aa8bc58a617856cc80d069ddc65c1382dbd1f
KANCHAM SUDHEER REDDY <sudheerreddy9346335294@gmail.com>
2025-12-11 11:53:50 +0530

    update Operation first Commit

---
+++ src/main/java/com/kodnest/updateDataHibernate/Student.java
@@ -0,0 +1,100 @@
+package com.kodnest.updateDataHibernate;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Student {
+
+	@Id
+	int id;
+
+	@Column
+	String name;
+
+	@Column
+	int marks;
+
+	@Column
+	String email;
+
+	public Student(int id, String name, int marks, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+		this.email = email;
+	}
+
+	public Student(String name, int marks, String email) {
+		super();
+		this.name = name;
+		this.marks = marks;
+		this.email = email;
+	}
+
+	public Student() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, marks, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(email, other.email) && id == other.id && marks == other.marks
+				&& Objects.equals(name, other.name);
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + ", email=" + email + "]";
+	}
+
+}
